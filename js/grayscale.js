de98f44141f897018efdfcd4a58dd7eaf19c7fab
GUNWOO LEE <gunman@GUNWOOui-MacBook-Pro.local>
2019-02-21 22:08:51 +0900

    동영상 플래이방법 변경, 소개 순서 변경

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,6 +1,11 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $('.play-button').on('click', function(e){
+      e.preventDefault();
+      $( '#' + $(this).data('modal-id') ).modal();
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
